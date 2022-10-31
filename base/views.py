1ef4342c1be854f088aee68136ce6d1cfb8fdc81
Kmehtanetmatic <Kunal.mehta@netmatic.tech>
2022-10-31 03:01:39 +0000

    Created user authentication form
    Updated the views.py with the login and logout feature
    Updated other bugs

--- base/views.py
+++ base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic
 from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .forms import RoomForm
 from django.db.models import Q
 from django.template import loader
@@ -13,6 +15,37 @@ from django.template import loader
 #     {'id': 2, 'name': 'Design With Me'},
 #     {'id': 3, 'name': 'Frontend Developer'},
 # ]
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username, password=password)
+        except:
+            messages.error(request, 'User does not exist')
+        
+            user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print(request.user.is_authenticated)
+            return redirect(home)
+        else:
+            print(messages.error)
+            messages.error(request, 'Username or password is incorrect')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    print(request)
+    logout(request)
+    print(request)
+    return redirect(home)
+
+
 
 def home(request):
     search = request.GET.get('search_for') if request.GET.get('search_for') != None else ''
@@ -52,18 +85,16 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
- 
     if request.method =='POST':
         form = RoomForm(request.POST, instance=room)
-
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('home')         
     context = {'forms': form}
     return render(request, 'base/room_form.html', context)
 
 def deleteRoom(request, pk):
-    room = Room.objects.get(id=pk)
+    room = Room.objects.get(id=pk) 
     print(request.META)
     if request.method == 'POST':
         room.delete()
